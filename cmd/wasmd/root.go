5b41ed83acedca4185d1ccacae77806cded106ab
Aravind Makam <aravind.makam@antiersolutions.com>
2022-06-06 14:02:16 +0530

    address prefic changes done

--- cmd/wasmd/root.go
+++ cmd/wasmd/root.go
@@ -44,9 +44,9 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 	encodingConfig := app.MakeEncodingConfig()
 
 	cfg := sdk.GetConfig()
-	cfg.SetBech32PrefixForAccount(app.Bech32PrefixAccAddr, app.Bech32PrefixAccPub)
-	cfg.SetBech32PrefixForValidator(app.Bech32PrefixValAddr, app.Bech32PrefixValPub)
-	cfg.SetBech32PrefixForConsensusNode(app.Bech32PrefixConsAddr, app.Bech32PrefixConsPub)
+	cfg.SetBech32PrefixForAccount("gemma", "gemma")
+	cfg.SetBech32PrefixForValidator("gemmaval", "gemmaval")
+	cfg.SetBech32PrefixForConsensusNode("gemmavalcons", "gemmavalcons")
 	cfg.SetAddressVerifier(wasmtypes.VerifyAddressLen())
 	cfg.Seal()
 
